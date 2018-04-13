7743b023208f5bcf74e8bbc9258073eebb4660d2
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-04-14 00:35:52 +0200

    protocol implementation ...

--- common/protocol/cli/src/generate.ts
+++ common/protocol/cli/src/generate.ts
@@ -1,3 +1,98 @@
 import { Builder } from '../../src/protocol.builder';
 
-console.log(process.argv);
+type TArgumentDescription = { description: string, hasParameter: boolean, args: Array<string>, errors: {[key:string]:string}};
+
+const ERRORS = {
+    noParameter: 'noParameter',
+    doubleParameter: 'doubleParameter'
+};
+
+const COMMANDS = {
+    output  : 'output',
+    source  : 'source',
+    help    : 'help',
+};
+
+const ARGUMENTS : {[key:string]: TArgumentDescription } = {
+    [COMMANDS.output]  : { 
+        description: 'Definition of output file. Format: ts (TypeScript)',
+        hasParameter: true,     
+        args: ['-o', '--outout', '--out'], 
+        errors: { [ERRORS.noParameter]: 'Key "-o" (--output, --out) expected file name after.', [ERRORS.doubleParameter]: 'Key "-o" (--output, --out) can be defined twice.'}
+    },
+    [COMMANDS.source]  : { 
+        description: 'Definition of input file. Expected format is JSON',
+        hasParameter: true,     
+        args: ['-s', '--source', '--src'], 
+        errors: { [ERRORS.noParameter]: 'Key "-s" (--source, --src) expected file name after.', [ERRORS.doubleParameter]: 'Key "-s" (--source, --src) can be defined twice.'}
+    },
+    [COMMANDS.help]    : { 
+        description: 'Show this message',
+        hasParameter: false,    
+        args: ['-h', '--help'], 
+        errors: {} 
+    }
+};
+
+function isItArgument(smth: string): boolean {
+    let result = false;
+    Object.keys(ARGUMENTS).forEach((command: string) => {
+        let description: TArgumentDescription = ARGUMENTS[command];
+        description.args.forEach((_arg: string)=>{
+            if (smth === _arg){
+                result = true;
+            }
+        });
+    });
+    return result;
+}
+
+const OUTPUTS = {
+    help: () => {
+        console.log(`Supported commands: \n${Object.keys(ARGUMENTS).map((command: string)=>{
+            let description: TArgumentDescription = ARGUMENTS[command];
+            return `${description.args.join(' | ')} - ${description.description};`;
+        }).join('\n')}`)
+    }
+};
+
+if (process.argv instanceof Array){
+    let commands : { [key: string]: string} = {};
+    process.argv.forEach((arg: string, index: number)=>{
+        Object.keys(ARGUMENTS).forEach((command: string)=>{
+            let description: TArgumentDescription = ARGUMENTS[command];
+            description.args.forEach((_arg: string)=>{
+                if (_arg === arg){
+                    if (description.hasParameter){
+                        if (process.argv[index + 1] !== void 0){
+                            if (!isItArgument(process.argv[index + 1]) && process.argv[index + 1].trim() !== ''){
+                                if (commands[command] === void 0){
+
+                                } else {
+                                    console.log(description.errors[ERRORS.doubleParameter]);
+                                    throw ERRORS.doubleParameter;
+                                }
+                            } else {
+                                console.log(description.errors[ERRORS.noParameter]);
+                                throw ERRORS.noParameter;
+                            }
+                        } else {
+                            console.log(description.errors[ERRORS.noParameter]);
+                            throw ERRORS.noParameter;
+                        }
+                    }
+                }
+            });
+        });
+    });
+    
+    if (Object.keys(commands).length > 0){
+        if (~Object.keys(commands).indexOf(COMMANDS.help)){
+            OUTPUTS.help();
+        } else if (~Object.keys(commands).indexOf(COMMANDS.source) && ~Object.keys(commands).indexOf(COMMANDS.output)){
+
+        }
+    } else {
+        OUTPUTS.help();
+    }
+}
