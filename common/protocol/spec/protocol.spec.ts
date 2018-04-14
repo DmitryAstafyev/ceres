6420196232335d80f59a546d947cd88c3a5d7f21
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-04-14 21:53:42 +0200

    protocol tests

--- common/protocol/spec/protocol.spec.ts
+++ common/protocol/spec/protocol.spec.ts
@@ -1,10 +1,15 @@
 /// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
+
 //./node_modules/.bin/jasmine-ts src/something.spec.ts
 
 import Logger from '../../platform/tools/tools.logger';
 import { Builder, Reader, IReaderResult, ProtocolJSONConvertor } from '../src/index';
+import * as FS from 'fs';
 
 const TEST_SOURCE_PROTOCOL_FILE = './spec/example/protocol.json';
+const TEST_DEST_PROTOCOL_FILE = './spec/example/protocol.ts';
+
 describe('[Test][platform][protocol]', () => {
     it('[Reader]', (done: Function)=>{
         const logger = new Logger('Test: Reader');
@@ -41,6 +46,18 @@ describe('[Test][platform][protocol]', () => {
                 const classStr = convertor.getImplementation();
                 expect(typeof classStr).toBe('string');
                 expect(classStr.length).toBeGreaterThan(0);
+                done();
+            }).catch((e)=>{
+                expect(e).toBeNull();
+                done();
+            });
+    });
+    it('[Builder]', (done: Function)=>{
+        const logger = new Logger('Test: Builder');
+        const builder: Builder = new Builder(TEST_SOURCE_PROTOCOL_FILE);
+        builder.build(TEST_DEST_PROTOCOL_FILE, true)
+            .then(() => {
+                expect(FS.existsSync(TEST_DEST_PROTOCOL_FILE)).toBe(true);
                 done();
             }).catch((e)=>{
                 expect(e).toBeNull();
