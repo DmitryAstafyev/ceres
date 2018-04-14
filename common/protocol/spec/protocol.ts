377efeacae5f631177c82ff19a32db98a0c4ef28
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-04-14 21:46:30 +0200

    protocol unit tests (basic)

---
+++ common/protocol/spec/protocol.ts
@@ -0,0 +1,51 @@
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+//./node_modules/.bin/jasmine-ts src/something.spec.ts
+
+import Logger from '../../platform/tools/tools.logger';
+import { Builder, Reader, IReaderResult, ProtocolJSONConvertor } from '../src/index';
+
+const TEST_SOURCE_PROTOCOL_FILE = './spec/example/protocol.json';
+describe('[Test][platform][protocol]', () => {
+    it('[Reader]', (done: Function)=>{
+        const logger = new Logger('Test: Reader');
+        const reader: Reader = new Reader(TEST_SOURCE_PROTOCOL_FILE);
+        reader.read()
+            .then((results: IReaderResult) => {
+                expect(typeof results).toBe('object');
+                expect(typeof results.body).toBe('object');
+                expect(typeof results.className).toBe('string');
+                expect(typeof results.json).toBe('object');
+                expect(typeof results.body.default).toBe('object');
+                expect(typeof results.body.definitions).toBe('object');
+                expect(typeof results.json.Message).toBe('object');
+                expect(typeof results.json.Message.default).toBe('object');
+                expect(typeof results.json.Message.definitions).toBe('object');
+                expect(typeof results.json.Message.default.event).toBe('object');
+                expect(typeof results.json.Message.default.request).toBe('object');
+                expect(typeof results.json.Message.definitions.Event).toBe('object');
+                expect(typeof results.json.Message.definitions.Request).toBe('object');
+                expect(typeof results.json.Message.definitions.Event.cases).toBe('object');
+                expect(typeof results.json.Message.definitions.Request.cases).toBe('object');
+                done();
+            }).catch((e)=>{
+                expect(e).toBeNull();
+                done();
+            });
+    });
+    it('[Convertor]', (done: Function)=>{
+        const logger = new Logger('Test: Convertor');
+        const reader: Reader = new Reader(TEST_SOURCE_PROTOCOL_FILE);
+        reader.read()
+            .then((results: IReaderResult) => {
+                const convertor: ProtocolJSONConvertor = new ProtocolJSONConvertor(results.json);
+                const classStr = convertor.getImplementation();
+                expect(typeof classStr).toBe('string');
+                expect(classStr.length).toBeGreaterThan(0);
+                done();
+            }).catch((e)=>{
+                expect(e).toBeNull();
+                done();
+            });
+    });
+
+});
--- common/protocol/src/index.ts
+++ common/protocol/src/index.ts
@@ -1,1 +1,4 @@
-export { Protocol } from './protocol';
+export { Reader, IReaderResult  } from './protocol.reader';
+export { Builder                } from './protocol.builder';
+export { ProtocolJSONConvertor  } from './protocol.JSON.convertor';
+export { ProtocolClassValidator } from './protocol.class.validator';
